0cea22ff27f72130dc2675a23f2b80cb8d3fbedc
Rio Edwards <rioredwards@gmail.com>
2023-03-19 20:04:29 -0700

    Add correct type definition to React Functional Component

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -2,6 +2,6 @@ interface ChildProps {
   color: string;
 }
 
-export const Child = ({ color }: ChildProps) => {
+export const Child: React.FC<ChildProps> = ({ color }) => {
   return <div>{`I am a child with the props: ${color}`}</div>;
 };
