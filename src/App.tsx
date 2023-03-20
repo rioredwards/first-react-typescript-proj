4d72bbee62690c4a156778410916db970db8247d
Rio Edwards <rioredwards@gmail.com>
2023-03-19 23:13:03 -0700

    Add some react events with functions that log their types

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import "./App.css";
 import Parent from "./props/Parent";
 import GuestList from "./state/GuestList";
 import UserSearch from "./state/UserSearch";
+import EventComponent from "./events/EventComponent";
 
 function App() {
   return (
     <div className="app">
+      <h1>React with Typescript</h1>
+      <EventComponent />
       <UserSearch />
       <GuestList />
       <Parent />
--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 const EventComponent = () => {
-  return <div>Event</div>;
+  const [event, setEvent] =
+    useState<React.ChangeEvent<HTMLInputElement> | null>(null);
+
+  function logChangeEvent(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log(e);
+    setEvent(e);
+  }
+
+  function logDragEvent(e: React.DragEvent<HTMLDivElement>) {
+    console.log(e);
+  }
+
+  return (
+    <div>
+      <h3>Event Component</h3>
+      <input onChange={logChangeEvent} type="text" />
+      <div>{event && event.target.value}</div>
+      <div onDrag={logDragEvent} draggable>
+        Drag me!
+      </div>
+    </div>
+  );
 };
 
 export default EventComponent;
